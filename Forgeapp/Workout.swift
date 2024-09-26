5b08417ce976aed6a5deab63eacee76f460dc3e1
Mark <roersma.m@gmail.com>
2024-09-26 10:46:14 +0200

    updated query

--- Forgeapp/Workout.swift
+++ Forgeapp/Workout.swift
@@ -8,27 +8,34 @@
 import Firebase
 import FirebaseFirestore
 
-struct Workout: Identifiable, Codable {
-    @DocumentID var workoutId: String?
-    let ownerUid: String
-    let timestamp: Timestamp
-    let title: String
-    let description: String?
-    let muscleGroups: [String]?
-    let exercises: [Exercise]?
-    
-    var id: String {
-        return workoutId ?? NSUUID().uuidString
+// Exercise Model
+struct Exercise: Identifiable, Codable {
+    @DocumentID var id: String? // Unique ID for each exercise
+    var name: String
+    var sets: Int
+    var reps: [Int]
+    var notes: String
+
+    init(name: String, sets: Int, reps: [Int], notes: String, superset: Bool? = false) {
+        self.name = name
+        self.sets = sets
+        self.reps = reps
+        self.notes = notes
     }
 }
 
-struct Exercise: Identifiable, Codable {
-    let id: String
-    let name: String
-    let sets: Int
-    let reps: [Int]
-    let notes: String
-    
-    // This could be used to store the ID from the external API
-    let externalApiId: String?
+// Workout Model
+struct Workout: Identifiable, Codable {
+    @DocumentID var id: String? // Unique ID for each workout
+    var title: String
+    var description: String
+    var muscleGroup: [String]
+    var exercises: [Exercise]? // Array to hold all exercises in the workout
+
+    init(title: String, description: String, muscleGroup: [String], exercises: [Exercise]? = nil) {
+        self.title = title
+        self.description = description
+        self.muscleGroup = muscleGroup
+        self.exercises = exercises
+    }
 }
